8e7d5b6bff5643e5374b42e96be9e09586047895
Francisco Valle <vallecesco@gmail.com>
2021-10-09 22:18:07 -0600

    Navegacion Chulada

--- src/components/views/home/HomeView.js
+++ src/components/views/home/HomeView.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Container, Row, Col, Navbar } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const HomeView = () => {
   return (
-    <Container fluid >
-      <Navbar bg="dark" variant="dark">
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
             <img
@@ -14,11 +14,21 @@ const HomeView = () => {
               height="30"
               className="d-inline-block align-top"
             />{" "}
-            React Bootstrap
+            React-Bootstrap
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="responsive-navbar-nav"
+          >
+            <Nav>
+              <Nav.Link>Inicio</Nav.Link>
+              <Nav.Link>Autos</Nav.Link>
+              <Nav.Link>Iniciar Sesion</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-    </Container>
   );
 };
 
