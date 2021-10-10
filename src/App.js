1db91e4a07178e96f8437caea2ad2b88d4e79e44
Francisco Valle <vallecesco@gmail.com>
2021-10-09 18:59:48 -0600

    Empezando Home con Bootstrap funcional

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React from 'react';
-import './App.css';
-import Saludo from './components/views/saludo/Saludo';
-import Home from './components/views/home/Home';
+import React from "react";
+import "./App.css";
+import Saludo from "./components/views/saludo/Saludo";
+import Home from "./components/views/home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Saludo />
-          <Home/>
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/components/views/home/HomeView.js
+++ src/components/views/home/HomeView.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
+
 const HomeView = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>1 of 1</Col>
-      </Row>
+    <Container fluid >
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://react-bootstrap.github.io/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            React Bootstrap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
