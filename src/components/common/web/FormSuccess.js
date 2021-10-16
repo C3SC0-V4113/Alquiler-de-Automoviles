afd7130033fba3881bb61869e80556903eb4d085
Francisco Valle <vallecesco@gmail.com>
2021-10-16 17:02:58 -0600

    Implementacion de Login y Registro

--- src/components/common/web/FormSuccess.js
+++ src/components/common/web/FormSuccess.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Form.css';
+import '../../views/signup/Signup.css';
 
 const FormSuccess = () => {
   return (
     <div className='form-content-right'>
       <h1 className='form-success'>¡Hemos recibido tu solicitud!</h1>
-      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+      <img className='form-img-2' src='https://freesvg.org/img/rocket-312767.png' alt='success-image' />
     </div>
   );
 };
--- src/components/navigation/WebNav.js
+++ src/components/navigation/WebNav.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {
-  Container,
-  Navbar,
-  Nav,
-} from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { Switch, Route, Link } from "react-router-dom";
 import Home from "../views/home/Home";
 import Vehiculos from "../views/vehiculos/Vehiculos";
 import Login from "../views/login/Login";
+import Signup from "../views/signup/Signup";
 
 const WebNav = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" style={{backgroundColor: '#1E2430'}} variant="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        style={{ backgroundColor: "#1E2430" }}
+        variant="dark"
+      >
         <Container>
           <Navbar.Brand as={Link} to="/">
             <img
@@ -30,9 +32,18 @@ const WebNav = () => {
             id="responsive-navbar-nav"
           >
             <Nav>
-              <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-              <Nav.Link as={Link} to="/Autos">Autos</Nav.Link>
-              <Nav.Link as={Link} to="/Login">Iniciar Sesion</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Inicio
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Autos">
+                Autos
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Login">
+                Iniciar Sesion
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Signup">
+                Registrarse
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -40,7 +51,8 @@ const WebNav = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/Autos" component={Vehiculos} />
-        <Route path="/Login" component={Login}/>
+        <Route path="/Login" component={Login} />
+        <Route path="/Signup" component={Signup} />
       </Switch>
     </>
   );
